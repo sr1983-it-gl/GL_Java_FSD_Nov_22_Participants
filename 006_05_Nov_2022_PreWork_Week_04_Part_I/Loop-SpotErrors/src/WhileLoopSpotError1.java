4bc26ce6fad6747607a9362c61482f868b3048e1
sr1983-it-gl <sr1983.it.gl@gmail.com>
2022-11-05 04:57:09 +0530

    Files

---
+++ 006_05_Nov_2022_PreWork_Week_04_Part_I/Loop-SpotErrors/src/WhileLoopSpotError1.java
@@ -0,0 +1,12 @@
+
+public class WhileLoopSpotError1 {
+	public static void main(String[] args) {
+		
+		boolean statusCheck = true;
+		
+		while (statusCheck) {
+			System.out.println("inside while...");
+		}
+		System.out.println("after while...");
+	}
+}
